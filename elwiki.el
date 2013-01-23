5dba7f007e18fec45c26fef785aebdcece56c6ef
Aidan Gauland <aidalgol@no8wireless.co.nz>
2013-01-24 12:09:06 +1300

    Licensing change
    Replaced Emacs logo (emacs.png) with Nic Ferrier's cloud gnus image
    because I couldn't find any licensing information on the Emacs logo.
    
    Relicensed Elwiki under the AGPL, updating the copyright notices
    accordingly.
    
    There is now a COPYING file stating the licensing of Elwiki, Nic's
    image, and the example creole page.

--- elwiki.el
+++ elwiki.el
@@ -2,15 +2,17 @@
 
 ;; Copyright (C) 2012  Nic Ferrier, Aidan Gauland
 
-;; Author: Nic Ferrier <nferrier@ferrier.me.uk>
+;; Author: Nic Ferrier <nferrier@ferrier.me.uk> and Aidan Gauland <aidalgol@no8wireless.co.nz>
 ;; Maintainer: Aidan Gauland <aidalgol@no8wireless.co.nz>
 ;; Created: 5th October 2010
 ;; Keywords: lisp, http, hypermedia
 
 ;; This file is NOT part of GNU Emacs.
 
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
+;; This file is part of Elwiki.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU Affero General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
 
@@ -19,7 +21,7 @@
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
 
-;; You should have received a copy of the GNU General Public License
+;; You should have received a copy of the GNU Affero General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Source code
