6eb4b2e15f3a3ab8df8e2276763b9ce01eae6097
Nic Ferier <nic@ferrier.me.uk>
2013-03-27 23:51:10 +0000

    convert outputs to html utf-8

--- elwiki.el
+++ elwiki.el
@@ -165,7 +165,9 @@ verbatim."
         ;; A specific page revision was requested, but we failed to get it.
         (elnode-send-404 httpcon "No such page revision.")
       (progn
-        (elnode-http-start httpcon 200 '("Content-type" . "text/html"))
+        (elnode-http-start
+         httpcon 200
+         '("Content-type" . "text/html; charset=utf-8"))
         (elwiki/render-page
          httpcon
          (or page-buffer wikipage)
@@ -179,7 +181,7 @@ verbatim."
 
 (defun elwiki-edit-page (httpcon wikipage &optional pageinfo preview)
   "Return an editor for WIKIPAGE via HTTPCON."
-  (elnode-http-start httpcon 200 '("Content-type" . "text/html"))
+  (elnode-http-start httpcon 200 '("Content-type" . "text/html; charset=utf-8"))
   (let* ((pageinfo (or pageinfo (elnode-http-pathinfo httpcon)))
          (page-name (elwiki/page-name pageinfo))
          (comment (elnode-http-param httpcon "comment"))
@@ -235,7 +237,7 @@ verbatim."
 
 (defun elwiki-history-page (httpcon wikipage)
   (elnode-error "Generating history page for %s" wikipage)
-  (elnode-http-start httpcon 200 '("Content-type" . "text/html"))
+  (elnode-http-start httpcon 200 '("Content-type" . "text/html; charset=utf-8"))
   (let ((page (string-to-int (or (elnode-http-param httpcon "page") "")))
         (commits-per-page 10))
    (elnode-send-html
